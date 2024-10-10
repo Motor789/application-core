60d180a1d88ece7da37ed4695312bc62ef80c98b
limengyang <limengyang@haomo.ai>
2024-10-10 15:30:04 +0800

    add external command

---
+++ modules/dreamview_plus/frontend/packages/dreamview-analysis/index.ts
@@ -0,0 +1,6 @@
+// src/index.ts
+import DreamviewAnalysis from './lib/dreamview-analysis';
+import {perfMonitor} from './lib/performance-monitor';
+import PerformanceMonitor from './react/PerformanceMonitor';
+
+export {DreamviewAnalysis, PerformanceMonitor, perfMonitor};
